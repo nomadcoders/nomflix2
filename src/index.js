8e3b05d91ea279b90dac719c6fe20880c9e08744
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-23 16:44:09 +0300

    Parcel Rocks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-import "./something.css";
-const plus = (a = 0, b = 7) => "AWESOME";
-console.log("asweo");
-plus(1, 1);
+document.querySelector("body").innerHTML = `<h1>Geolocation 2000.</h1>
+<button class="js-locateMeBtn">Locate me!</button>
+<div class="js-output"></div>`;
+
+const locateMeBtn = document.querySelector(".js-locateMeBtn"),
+  output = document.querySelector(".js-output");
+
+const createMapImage = (lat, lng) => {
+  const imageURL = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&size=500x500&zoom=12&markers=color:red|label:U|${lat},${lng}&maptype=hybrid&key=AIzaSyAZItvexravC2icdhsjSGHA6Ovbk2ROwmc`;
+  const image = new Image();
+  image.src = imageURL;
+  output.appendChild(image);
+};
+
+const handleGeoError = () => {
+  output.innerHTML = "<h2>Can't locate you 😞</h2>";
+};
+
+const handleGeoSuccess = position => {
+  const {
+    coords: { latitude, longitude }
+  } = position;
+  output.innerHTML = "<h2>Got you!</h2>";
+  createMapImage(latitude, longitude);
+};
+
+const getLocation = () => {
+  output.innerHTML = "<h2>Locating you...</h2>";
+  const geoOptions = {
+    enableHighAccuracy: true
+  };
+  navigator.geolocation.getCurrentPosition(
+    handleGeoSuccess,
+    handleGeoError,
+    geoOptions
+  );
+};
+
+locateMeBtn.addEventListener("click", getLocation);
